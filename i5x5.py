41261ee85ec56858927773d4e8d7adfd2c212584
Kitajima Takumi <m22050@metro-cit.ac.jp>
2024-11-24 15:42:40 +0900

    mac

--- i5x5.py
+++ i5x5.py
@@ -1,7 +1,33 @@
 import random
 
-def trans_mpp2(self,before_wide):
-    w = before_wide
+def normal(ls):
+    if ls == [] or ls == [[]]:
+        return True
+    n = len(ls[0])
+    out = True
+    for i in ls:
+        if len(i) != n:
+            out = False
+    return out
+
+def trans_mpp2(ls):
+    if not(normal(ls)):
+        return [[]]
+    if ls == [[]] or ls == []:
+        return [[]]
+    h = len(ls[0])
+    w = len(ls)
+    out = []
+    for i in range(h):
+        out.append([])
+    print(h,out)
+    n = 0
+    for i in range(h):
+        for j in range(w):
+            print(out,i,ls,j,i)
+            out[i].insert(0,ls[j][i])
+    return out
+
 
 """
 012
@@ -12,8 +38,18 @@ def trans_mpp2(self,before_wide):
 52
 """
 
-def mirror(self,wide):
-    pass
+def mirror(ls):
+    if not(normal(ls)):
+        return [[]]
+    if ls == [[]] or ls == []:
+        return [[]]
+    out = []
+    for i in ls:
+        semiout = []
+        for j in i:
+            semiout.insert(0,j)
+        out.append(semiout)
+    return out
 
 """
 123
@@ -21,6 +57,18 @@ def mirror(self,wide):
 
 321
 654
+"""
+def symme(self):
+    pass
+
+"""
+123
+456
+789
+
+32123
+65456
+98789
 """
 
 """
--- test_i5x5.py
+++ test_i5x5.py
@@ -1,1 +1,58 @@
 from i5x5 import *
+
+def test_normal():
+    assert normal([]) == True
+    assert normal([[],[]]) == True
+    assert normal([[1]]) == True
+    assert normal([[1,2],[3,4]]) == True
+    assert normal([[1,2,3],[4,5,6],[7,8,9]]) == True
+
+    assert normal([[1],[]]) == False
+    assert normal([[],[1]]) == False
+    assert normal([[1],[3,4]]) == False
+    assert normal([[1,2,3],[4,6],[7,8,9]]) == False
+
+
+def test_trans_mpp2():
+    assert trans_mpp2([]) == [[]]
+    assert trans_mpp2([[]]) == [[]]
+
+    assert trans_mpp2([[1]]) == [[1]]
+    assert trans_mpp2([
+        [1],
+        [2]]) == [[2,1]]
+    assert trans_mpp2([
+        [1,2],
+        [3,4]]) == [
+            [3,1],
+            [4,2]]
+    assert trans_mpp2([
+        [1,2],
+        [3,4],
+        [5,6],
+        [7,8]]) == [
+            [7,5,3,1],
+            [8,6,4,2]]
+
+def test_mirror():
+    assert mirror([]) == [[]]
+    assert mirror([[]]) == [[]]
+
+    assert mirror([[1]]) == [[1]]
+    assert mirror([
+        [1],
+        [2]]) == [
+            [1],
+            [2]]
+    assert mirror([
+        [1,2],
+        [3,4]]) == [
+            [2,1],
+            [4,3]]
+    assert mirror([
+        [1,2,3],
+        [4,5,6],
+        [7,8,9]])==[
+            [3,2,1],
+            [6,5,4],
+            [9,8,7]]
